9b84c5b5151dcd1f2cc1a863c6a2b2639d2a11f0
Aditybot <155868050+Aditybot@users.noreply.github.com>
2024-05-26 15:18:31 +0530

    Update thumbnails.py

--- DAXXMUSIC/utils/thumbnails.py
+++ DAXXMUSIC/utils/thumbnails.py
@@ -145,7 +145,7 @@ async def get_thumb(videoid):
     circle_radius = 10 
     circle_position = (end_point_red[0], end_point_red[1])
     #draw.ellipse([circle_position[0] - circle_radius, circle_position[1] - circle_radius,
-                  circle_position[0] + circle_radius, circle_position[1] + circle_radius], fill="red")
+                  #circle_position[0] + circle_radius, circle_position[1] + circle_radius], fill="red")
     #draw.text((text_x_position, 400), "00:00", (255, 255, 255), font=arial)
     #draw.text((1080, 400), duration, (255, 255, 255), font=arial)
     
